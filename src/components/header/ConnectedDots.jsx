68cd65602161ef5e00c5437a1ca01497d4be3d83
dilshod2007 <dilshodumbarov707@gmail.com>
2024-10-01 18:45:37 +0500

    finish\

--- src/components/header/ConnectedDots.jsx
+++ src/components/header/ConnectedDots.jsx
@@ -1,5 +1,5 @@
 import React, { useRef, useEffect } from "react";
-import "./connectedDots.css";
+import "../header/ConnectedDots.css";
 
 const ConnectedDots = () => {
   const canvasRef = useRef(null);
